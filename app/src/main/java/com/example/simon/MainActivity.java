8d190b63697372ad68255bec95e9260668925fc6
blc_ele <boulocheeleonore28@gmail.com>
2024-02-11 19:56:21 +0100

    "new version : levels + colors"

--- app/src/main/java/com/example/simon/MainActivity.java
+++ app/src/main/java/com/example/simon/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.simon;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.graphics.Color;
-
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 
@@ -25,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private List<Integer> lstCasesUser;
 
     private int[] tabIdCouleurs;
+
+
+    private int     temps;
+    private double coeffTemps;
+    private int    niveauActuel;
 
 
     private boolean debutPartieJoueur;
@@ -66,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+
         this.debutPartieJoueur = false;
         this.finPartie         = true;
-
 
 
         TextView textView;
@@ -78,10 +82,81 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textViewScore);
         textView.setText("Score : " + this.score);
+
+
+        Button buttonNiveau2 = (Button) findViewById(R.id.buttonNiveau2);
+        buttonNiveau2.setBackground(null);
+        buttonNiveau2.setBackgroundResource( R.drawable.button_niveau_non_select );
+
+
+        this.temps      = 500;
+        this.coeffTemps = 1;
+
+        this.niveauActuel = 1;
     }
 
 
-    public void debutJeu (View view) { this.genererCase(); }
+    public void debutJeu (View view)
+    {
+        if ( this.finPartie )
+        {
+            this.genererCase();
+            this.finPartie = false;
+
+            if ( this.niveauActuel == 1 ) this.niveau1();
+            else                          this.niveau2();
+        }
+    }
+
+
+    public void clickNiveau1 (View view)
+    {
+        if ( this.finPartie )
+        {
+            this.niveauActuel = 1;
+            this.niveau1();
+        }
+    }
+
+
+    public void clickNiveau2 (View view)
+    {
+        if ( this.finPartie )
+        {
+            this.niveauActuel = 2;
+            this.niveau2();
+        }
+    }
+
+
+    public void niveau1 ()
+    {
+        this.temps      = 500;
+        this.coeffTemps = 1;
+
+        Button buttonNiveau1 = (Button) findViewById(R.id.buttonNiveau1);
+        buttonNiveau1.setBackground(null);
+        buttonNiveau1.setBackgroundResource( R.drawable.button );
+
+        Button buttonNiveau2 = (Button) findViewById(R.id.buttonNiveau2);
+        buttonNiveau2.setBackground(null);
+        buttonNiveau2.setBackgroundResource( R.drawable.button_niveau_non_select );
+    }
+
+
+    public void niveau2 ()
+    {
+        this.temps      = 500;
+        this.coeffTemps = 0.95;
+
+        Button buttonNiveau1 = (Button) findViewById(R.id.buttonNiveau1);
+        buttonNiveau1.setBackground(null);
+        buttonNiveau1.setBackgroundResource( R.drawable.button_niveau_non_select );
+
+        Button buttonNiveau2 = (Button) findViewById(R.id.buttonNiveau2);
+        buttonNiveau2.setBackground(null);
+        buttonNiveau2.setBackgroundResource( R.drawable.button );
+    }
 
 
     private void genererCase ()
@@ -96,7 +171,11 @@ public class MainActivity extends AppCompatActivity {
     private void modifierCoul()
     {
         final Handler handler = new Handler();
-        final int delayBetweenColors = 200;
+        final int delayBetweenColors = (int)( this.temps * this.coeffTemps );
+
+
+        this.casesAViolet();
+
 
         // Start with the first index
         displayColorRecursive(handler, 0, delayBetweenColors);
@@ -118,19 +197,69 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, delayBetweenColors * 1); // Délai entre chaque changement
 
+
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
+                    if ( (int)( temps * coeffTemps ) > 100 ) temps = (int)( temps * coeffTemps );
                     // Retour à la couleur par défaut ou toute autre couleur souhaitée
                     textView.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.violet));
                     // Continue with the next color after a delay
-                    displayColorRecursive(handler, index + 1, delayBetweenColors);
+                    displayColorRecursive(handler, index + 1, temps   );
                 }
             }, delayBetweenColors * 2 + 100); // Introduce a small additional delay for resetting
         }
 
         else
-            this.debutPartieJoueur = true;
+        {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    casesACoul();
+
+                    debutPartieJoueur = true;
+                }
+            }, delayBetweenColors * 3 + 100); // Introduce a small additional delay for resetting
+        }
+    }
+
+
+    private void casesAViolet()
+    {
+        TextView[] tabTextView = new TextView[4];
+
+        tabTextView[0] = (TextView) findViewById(R.id.textView1);
+        tabTextView[1] = (TextView) findViewById(R.id.textView2);
+        tabTextView[2] = (TextView) findViewById(R.id.textView3);
+        tabTextView[3] = (TextView) findViewById(R.id.textView4);
+
+        for ( TextView t : tabTextView )
+        {
+            t.setBackground(null);
+            t.setBackgroundResource( R.color.violet );
+        }
+    }
+
+
+    private void casesACoul ()
+    {
+        TextView textView;
+
+        textView = (TextView) findViewById(R.id.textView1);
+        textView.setBackground(null);
+        textView.setBackgroundResource( R.drawable.text_view_bleu );
+
+        textView = (TextView) findViewById(R.id.textView2);
+        textView.setBackground(null);
+        textView.setBackgroundResource( R.drawable.text_view_vert );
+
+        textView = (TextView) findViewById(R.id.textView3);
+        textView.setBackground(null);
+        textView.setBackgroundResource( R.drawable.text_view_jaune );
+
+        textView = (TextView) findViewById(R.id.textView4);
+        textView.setBackground(null);
+        textView.setBackgroundResource( R.drawable.text_view_rouge );
     }
 
 
@@ -179,6 +308,11 @@ public class MainActivity extends AppCompatActivity {
 
             valCorrectes = false;
         }
+
+
+        if ( !valCorrectes )
+            this.casesAViolet();
+
 
         if ( valCorrectes && this.lstCasesGenerees.size() == this.lstCasesUser.size() )
         {
